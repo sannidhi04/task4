3dd513062e7e143394b83cc5fddb501c0e608c17
sannidhi <reddysannidhi04@gmail.com>
2022-09-25 11:36:02 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require("http");
+const fs = require("fs");
+fs.readFile("home.html", (err, home) => {
+    console.log(home.toString());
+  });
+  fs.readFile("home.html", (err, home) => {
+    if (err) {
+      throw err;
+    }
+    http
+      .createServer((request, response) => {
+        response.writeHeader(200, { "Content-Type": "text/html" });
+        response.write(home);
+        response.end();
+      })
+      .listen(5000);
+  });
